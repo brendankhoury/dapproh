fa21f90c3d449dfd37c649655116a98257b07850
Brendan Khoury <53023770+brendankhoury@users.noreply.github.com>
2021-11-23 17:10:54 -0800

    Functional encryption on user_data

--- lib/controllers/user_data.dart
+++ lib/controllers/user_data.dart
@@ -13,6 +13,7 @@ import 'package:hive/hive.dart';
 import 'package:skynet/skynet.dart';
 import 'package:steel_crypt/steel_crypt.dart';
 import 'dart:convert' as ConvertPack;
+import '../util/enc_util.dart';
 
 class UserDataController extends ChangeNotifier {
   late SkynetUser user;
@@ -20,22 +21,16 @@ class UserDataController extends ChangeNotifier {
 
   SkynetClient skynetClient = SkynetClient();
   Feed feed = Feed();
+
   void initUser() async {
-    CryptKey keyGen = CryptKey();
-    final pdfk2 = Pbkdf2(macAlgorithm: Hmac.sha256(), iterations: 1000, bits: 256);
-    // if (Hive.box("configuration").get("mnemonic"))
     String mnemonic = Hive.box("configuration").get("mnemonic");
-    final stringKey = await pdfk2.deriveKey(secretKey: SecretKey(mnemonic.codeUnits), nonce: []);
-    final secretKeyData = await stringKey.extract();
-    debugPrint("Key: ${ConvertPack.base64Encode(secretKeyData.bytes)}");
-    debugPrint("Ky2: ${keyGen.genFortuna()}");
+    String key = await EncUtil.mnemonicToKey(mnemonic);
 
     assert(validateMnemonic(mnemonic));
 
     // Check if the private user exists in box.
     user = await SkynetUser.fromMySkySeedRaw(mnemonicToSeed(mnemonic));
 
-    SkyFile privateUserFeedRaw;
     try {
       debugPrint(mnemonicToSeedHex(mnemonic));
       await skynetClient.skydb.getFile(user, PRIVATE_USER_FEED_KEY).then(onPrivateContentRetrieve); //1.onError(onContentRetrievalFailure);
@@ -44,19 +39,22 @@ class UserDataController extends ChangeNotifier {
     }
   }
 
-  void onPrivateContentRetrieve(content) {
+  void onPrivateContentRetrieve(content) async {
     if (content.asString == null) {
       debugPrint("Peepo Panic, null file content");
       debugPrint("${content.asString}");
     } else {
-      debugPrint("privateUserFeed: ${content.asString}");
       Box configBox = Hive.box("configuration");
 
       String public_iv16 = configBox.get("publicIv16");
+      debugPrint("privateUserFeed: ${content.asString}");
 
-      AesCrypt encryption = AesCrypt(key: mnemonicToEntropy(configBox.get("mnemonic")), padding: PaddingAES.pkcs7);
+      AesCrypt encryption = AesCrypt(key: await EncUtil.mnemonicToKey(configBox.get("mnemonic")), padding: PaddingAES.pkcs7);
+      debugPrint("privateUserFeed: post encryption");
 
       String decryptedContent = encryption.gcm.decrypt(enc: content.asString, iv: public_iv16);
+      debugPrint("privateUserFeed: decrypted content ${decryptedContent}");
+
       Map<String, dynamic> decodedJson = jsonDecode(decryptedContent);
 
       PrivateUser privateUserFeed = PrivateUser.fromJson(decodedJson);
@@ -65,17 +63,18 @@ class UserDataController extends ChangeNotifier {
   }
 
   void onContentRetrievalFailure(error) async {
-    debugPrint("Failure ${error.toString()}\n${error.runtimeType}\n\n");
+    debugPrint("Failure ${error.toString()}\n Runtimetype: ${error.runtimeType}");
     if (error.toString() == "Exception: not found") {
       debugPrint("Not found exception, creating a new file");
       Box configBox = Hive.box("configuration");
       CryptKey keyGen = CryptKey();
 
       PrivateUser privateUser = PrivateUser({}, [], keyGen.genFortuna());
-      AesCrypt encryption = AesCrypt(key: mnemonicToSeedHex(configBox.get("mnemonic")), padding: PaddingAES.pkcs7);
+      AesCrypt encryption = AesCrypt(key: await EncUtil.mnemonicToKey(configBox.get("mnemonic")), padding: PaddingAES.pkcs7);
       String privateUserString = jsonEncode(privateUser);
       configBox.put("privateUser", privateUserString);
       String public_iv16 = keyGen.genDart();
+
       configBox.put("publicIv16", public_iv16);
       await skynetClient.skydb
           .setFile(user, PRIVATE_USER_FEED_KEY,
@@ -90,7 +89,7 @@ class UserDataController extends ChangeNotifier {
                   content: Uint8List.fromList(utf8.encode(encryption.gcm.encrypt(inp: privateUserString, iv: public_iv16))),
                   filename: "private.txt",
                   type: "text/plain"))
-          .then((value) => debugPrint("$value file set"))
+          .then((value) => debugPrint("$value, private.txt file set"))
           .onError((error, stackTrace) => debugPrint("${error.toString()} ${stackTrace.toString()}"));
     } else {
       debugPrint("Exception not identified: ${error.toString()}");
